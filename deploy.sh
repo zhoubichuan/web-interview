afe3db497dcbe2d37d2ddd8adeba7ccb65d1d19c
zhoubichuan <zhoubichuan@icloud.com>
2020-01-01 22:49:18 +0800

    master

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,13 @@
 # 确保脚本抛出遇到的错误
 set -e
 
+git init
+git add -A
+git commit -m 'master'
+git remote add origin https://github.com/zhoubichuan/Web-Interview.git
+git pull origin master
+git push -u origin master
+git push origin master
 # 生成静态文件
 npm run src:build
 
@@ -15,6 +22,8 @@ git add -A
 git commit -m 'deploy'
 
 git remote add origin https://github.com/zhoubichuan/Web-Interview.git
+git checkout origin/gh-pages
+git checkout -b gh-pages
 git pull origin gh-pages
 git push -u origin gh-pages
 git push origin gh-pages 
