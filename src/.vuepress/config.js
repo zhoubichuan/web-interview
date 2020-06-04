eecb42d2c62019cd176f77fda9133b5d2729cc2e
zhoubichuan <zhoubichuan@icloud.com>
2020-06-04 23:22:26 +0800

    master

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -137,7 +137,65 @@ module.exports = {
           },
           {
             text: '大厂面试题',
-            link: '/page/article/'
+            link: '/article/',
+            items: [{
+              text: '阿里巴巴',
+              link: '/article/阿里巴巴',
+            }, {
+              text: '饿了么',
+              link: '/article/饿了么',
+            }, {
+              text: '百分点',
+              link: '/article/百分点',
+            }, {
+              text: '宝宝树',
+              link: '/article/宝宝树',
+            }, {
+              text: '滴滴',
+              link: '/article/滴滴',
+            }, {
+              text: '兑吧',
+              link: '/article/兑吧',
+            }, {
+              text: '海风教育',
+              link: '/article/宝海风教育',
+            }, {
+              text: '海康威视',
+              link: '/article/海康威视',
+            }, {
+              text: '沪江',
+              link: '/article/沪江',
+            }, {
+              text: '今日头条',
+              link: '/article/今日头条',
+            }, {
+              text: '酷家乐',
+              link: '/article/酷家乐',
+            }, {
+              text: '蘑菇街',
+              link: '/article/蘑菇街',
+            }, {
+              text: '寺库',
+              link: '/article/寺库',
+            }, {
+              text: '挖财',
+              link: '/article/挖财',
+            }, {
+              text: '网易',
+              link: '/article/网易',
+            }, {
+              text: '微医',
+              link: '/article/微医',
+            }, {
+              text: '喜马拉雅',
+              link: '/article/喜马拉雅',
+            }, {
+              text: '携程',
+              link: '/article/携程',
+            }, {
+              text: '有赞',
+              link: '/article/有赞',
+            }]
           }
         ],
         sidebar: {
@@ -239,130 +297,130 @@ module.exports = {
             }
           ],
           // 文档侧边栏 跳转到不同页面
-          '/page/base/': [{
+          '/base/': [{
             title: '一、标签及样式',
             collapsable: false,
             children: ['', 'CSS']
           }],
-          '/page/article/': [{
-              title: '阿里巴巴',
+          '/article/': [{
+              title: '阿里相关',
               collapsable: false, // 让一个组永远都是展开状态
               children: [
-                '',
-                'elem',
-                'mogujie',
+                '阿里巴巴',
+                '蘑菇街',
+                '饿了么'
               ]
             },
             {
-              title: 'baobaoshu',
+              title: '今日头条',
               collapsable: false, // 让一个组永远都是展开状态
               children: [
-                'baobaoshu',
+                '今日头条',
               ]
             },
             {
               title: '滴滴',
               collapsable: false, // 让一个组永远都是展开状态
               children: [
-                'didi',
+                '滴滴',
               ]
             },
             {
-              title: 'duiba',
+              title: '兑吧',
               collapsable: false, // 让一个组永远都是展开状态
               children: [
-                'duiba',
+                '兑吧',
               ]
             },
             {
-              title: 'haifendian',
+              title: '百分点',
               collapsable: false, // 让一个组永远都是展开状态
               children: [
-                'haifendian',
+                '百分点',
               ]
             },
             {
               title: '海风教育',
               collapsable: false, // 让一个组永远都是展开状态
               children: [
-                'haifengjiaoyu',
+                '海风教育',
               ]
             },
             {
               title: '海康威视',
               collapsable: false, // 让一个组永远都是展开状态
               children: [
-                'haikangweishi',
+                '海康威视',
               ]
             },
             {
               title: '沪江',
               collapsable: false, // 让一个组永远都是展开状态
               children: [
-                'hujiang',
+                '沪江',
               ]
             },
             {
               title: '酷家乐',
               collapsable: false, // 让一个组永远都是展开状态
               children: [
-                'kujiale',
+                '酷家乐',
               ]
             },
             {
               title: '寺库',
               collapsable: false, // 让一个组永远都是展开状态
               children: [
-                'siku',
+                '寺库',
               ]
             },
             {
-              title: '头条',
+              title: '今日头条',
               collapsable: false, // 让一个组永远都是展开状态
               children: [
-                'toutiao',
+                '今日头条',
               ]
             },
             {
               title: '挖财',
               collapsable: false, // 让一个组永远都是展开状态
               children: [
-                'wacai',
+                '挖财',
               ]
             },
             {
               title: '网易',
               collapsable: false, // 让一个组永远都是展开状态
               children: [
-                'wangyi',
+                '网易',
               ]
             },
             {
-              title: 'weiyi',
+              title: '微医',
               collapsable: false, // 让一个组永远都是展开状态
               children: [
-                'weiyi',
+                '微医',
               ]
             },
             {
-              title: 'xiecheng',
+              title: '携程',
               collapsable: false, // 让一个组永远都是展开状态
               children: [
-                'xiecheng',
+                '携程',
               ]
             },
             {
               title: '喜马拉雅',
               collapsable: false, // 让一个组永远都是展开状态
               children: [
-                'ximalaya',
+                '喜马拉雅',
               ]
             },
             {
               title: '有赞',
               collapsable: false, // 让一个组永远都是展开状态
               children: [
-                'youzan',
+                '有赞',
               ]
             },
           ]
