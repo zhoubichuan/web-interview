f19fa9dc9d1f0334db5cbab61bb68846d3b0392c
zhoubichuan <zhoubichuan@icloud.com>
2022-02-19 15:43:20 +0800

    master

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -1,261 +1,51 @@
 module.exports = {
-  // 基础配置
   title: 'web面试题精选',
   description: '种一棵树最好的时间是十年前 其次是现在',
   base: '/web-interview/', // 部署站点的基础路径
   port: 3009,
-  dest: 'dist', // 指定 vuepress build 的输出目录
-  serviceWorker: true, // pwa
-
+  dest: 'dist', // 指定 vuepress 的输出目录
+  markdown: {
+    lineNumbers: true // 代码块显示行号
+  },
+  plugins: {
+    'demo-container': true,
+    // 名称：@vuepress/plugin-pwa 网页内容有更新的时候有刷新按钮。可以把网页保存到桌面，当一个app一样
+    '@vuepress/pwa': {
+      serviceWorker: true,
+      updatePopup: true
+    },
+    // 名称：@vuepress/plugin-back-to-top 效果：文章看到下面的时候，点击一个图标会回到顶部
+    '@vuepress/back-to-top': true,
+    '@vuepress/medium-zoom': true,
+    '@vuepress/nprogress': true,
+    // 名称：@vuepress/plugin-active-header-links 效果：页面滚动时自动激活侧边栏链接的插件，效果就是右边内容滚动的时候，看到哪里了，左侧菜单会自动高亮显示当前看的目录。
+    '@vuepress/active-header-links': {
+      sidebarLinkSelector: '.sidebar-link',
+      headerAnchorSelector: '.header-anchor'
+    },
+    "@vssue/vuepress-plugin-vssue": {
+      platform: 'github', //v3的platform是github，v4的是github-v4
+      locale: 'zh', //语言
+      // 其他的 Vssue 配置
+      owner: 'zhoubichuan', //github账户名
+      repo: 'web-vue', //github一个项目的名称
+      clientId: 'Iv1.2923ba5d4de48a3c', //注册的Client ID
+      clientSecret: '110210', //注册的Client Secret
+      autoCreateIssue: true // 自动创建评论，默认是false，最好开启，这样首次进入页面的时候就不用去点击创建评论的按钮了。
+    },
+    "vuepress-plugin-boxx": ["vuepress-plugin-boxx"]
+  },
   // 主题配置
   themeConfig: {
     editLinks: true,
     docsDir: 'src', // 假如文档不是放在仓库的根目录下
-    locales: {
-      '/': {
-        docsBranch: 'master', // 假如文档放在一个特定的分支下
-        editLinkText: '在github上编辑此页',
-        lastUpdated: '上次更新', // 获取每个文件最后一次 git 提交的时间戳
-        nav: [{
-          text: '前端面试题',
-          items: [
-            {
-              text: '一.HTML部分',
-              link: '/1.front-end/1.html/1.index',
-            },
-            {
-              text: '二.CSS部分',
-              link: '/1.front-end/2.css/1.index',
-            },
-            {
-              text: '三.JavaScript部分',
-              link: '/1.front-end/3.javascript/1.index',
-            },
-            {
-              text: '四.浏览器部分',
-              link: '/1.front-end/4.browser/1.index',
-            },
-            {
-              text: '五.框架【vue】部分',
-              link: '/1.front-end/5.vue/1.index',
-            },
-            {
-              text: '六.框架【react】部分',
-              link: '/1.front-end/6.react/1.index',
-            },
-            {
-              text: '七.框架【小程序】部分',
-              link: '/1.front-end/7.小程序/1.index',
-            },
-          ],
-        },
-        {
-          text: '高级面试题',
-          items: [
-            {
-              text: '一.JavaScript',
-              link: '/senior/javascript/1.advanc',
-            },
-            {
-              text: '二.网络',
-              link: '/senior/network/1.UDP',
-            },
-          ],
-        },
-        {
-          text: '后端面试题',
-          link: '/2.back-end/node',
-          items: [
-            {
-              text: '一、Node',
-              link: '/2.back-end/1.node',
-            },
-            {
-              text: '二、Express',
-              link: '/2.back-end/2.express',
-            },
-            {
-              text: '三、Koa',
-              link: '/2.back-end/3.koa',
-            },
-            {
-              text: '四、Egg',
-              link: '/2.back-end/4.egg',
-            }]
-        },
-        {
-          text: '大厂面试题',
-          link: '/largeFactories/',
-          items: [
-            {
-              text: '阿里巴巴',
-              link: '/largeFactories/阿里巴巴',
-            },
-            {
-              text: '饿了么',
-              link: '/largeFactories/饿了么',
-            },
-            {
-              text: '百分点',
-              link: '/largeFactories/百分点',
-            },
-            {
-              text: '宝宝树',
-              link: '/largeFactories/宝宝树',
-            },
-            {
-              text: '滴滴',
-              link: '/largeFactories/滴滴',
-            },
-            {
-              text: '兑吧',
-              link: '/largeFactories/兑吧',
-            },
-            {
-              text: '海风教育',
-              link: '/largeFactories/宝海风教育',
-            },
-            {
-              text: '海康威视',
-              link: '/largeFactories/海康威视',
-            },
-            {
-              text: '沪江',
-              link: '/largeFactories/沪江',
-            },
-            {
-              text: '今日头条',
-              link: '/largeFactories/今日头条',
-            },
-            {
-              text: '酷家乐',
-              link: '/largeFactories/酷家乐',
-            },
-            {
-              text: '蘑菇街',
-              link: '/largeFactories/蘑菇街',
-            },
-            {
-              text: '寺库',
-              link: '/largeFactories/寺库',
-            },
-            {
-              text: '挖财',
-              link: '/largeFactories/挖财',
-            },
-            {
-              text: '网易',
-              link: '/largeFactories/网易',
-            },
-            {
-              text: '微医',
-              link: '/largeFactories/微医',
-            },
-            {
-              text: '喜马拉雅',
-              link: '/largeFactories/喜马拉雅',
-            },
-            {
-              text: '携程',
-              link: '/largeFactories/携程',
-            },
-            {
-              text: '有赞',
-              link: '/largeFactories/有赞',
-            }]
-        },
-        {
-          text: '面试题',
-          link: '/myInterview/20210131'
-        }
-        ],
-        sidebar: {
-          '/1.front-end/1.html/': [
-            '1.index',
-            '2.htmI',
-            '3.html5'
-          ],
-          '/1.front-end/2.css/': [
-            '1.index',
-            '2.css',
-            '3.css3'
-          ],
-          '/1.front-end/3.javascript/': [
-            '1.index',
-            '1.base',
-            '2.senior',
-            '3.es6',
-            '4.safe',
-            '5.performance',
-            '6.design',
-            '7.monitor',
-            '8.cal',
-            '9.handle'
-          ],
-          '/1.front-end/4.browser/': [
-            '1.index',
-            '2.render',
-            '3.cache',
-            '5.v8',
-            '6.render',
-            '7.cache',
-            '11.network'
-          ],
-          '/1.front-end/5.vue/': [
-            '1.index',
-            '2.advanc'
-          ],
-          '/1.front-end/6.react/': [
-            '1.index',
-            '2.advanc'
-          ],
-          '/senior/javascript/': [
-            '1.advanc',
-            '2.design',
-            '3.dataStructure',
-            '4.algorithm'
-          ],
-          '/senior/network/': [
-            '1.UDP',
-            '2.TCP',
-            '3.HTTPandTLS',
-            '4.HTTP2andHTTP3'
-          ],
-          '/senior/browser/': [
-            '1.cache',
-            '2.render'
-          ],
-          '/2.back-end/': [
-            '1.node',
-            '2.express',
-            '3.koa',
-            '4.egg'
-          ],
-          '/largeFactories/': [
-            '阿里巴巴',
-            '蘑菇街',
-            '饿了么',
-            '今日头条',
-            '滴滴',
-            '兑吧',
-            '百分点',
-            '海风教育',
-            '海康威视',
-            '沪江',
-            '酷家乐',
-            '寺库',
-            '挖财',
-            '网易',
-            '微医',
-            '携程',
-            '喜马拉雅',
-            '有赞'
-          ],
-          '/myInterview/': [
-            '20210131'
-          ]
-        }
-      }
-    }
-  }
+    docsBranch: 'master', // 假如文档放在一个特定的分支下
+    editLinks: true, // 启用编辑
+    editLinkText: '在github上编辑此页',
+    sidebarDepth: 2,
+    lastUpdated: '上次更新', // 获取每个文件最后一次 git 提交的时间戳
+    nav: require('./nav.js'),
+    sidebar: require('./sidebar.js'),
+    searchMaxSuggestoins: 10
+  },
 }
---
+++ src/.vuepress/sidebar.js
@@ -0,0 +1,87 @@
+const sidebar = {
+    '/1.front-end/1.html/': [
+      '1.index',
+      '2.htmI',
+      '3.html5'
+    ],
+    '/1.front-end/2.css/': [
+      '1.index',
+      '2.css',
+      '3.css3'
+    ],
+    '/1.front-end/3.javascript/': [
+      '1.index',
+      '1.base',
+      '2.senior',
+      '3.es6',
+      '4.safe',
+      '5.performance',
+      '6.design',
+      '7.monitor',
+      '8.cal',
+      '9.handle'
+    ],
+    '/1.front-end/4.browser/': [
+      '1.index',
+      '2.render',
+      '3.cache',
+      '5.v8',
+      '6.render',
+      '7.cache',
+      '11.network'
+    ],
+    '/1.front-end/5.vue/': [
+      '1.index',
+      '2.advanc'
+    ],
+    '/1.front-end/6.react/': [
+      '1.index',
+      '2.advanc'
+    ],
+    '/senior/javascript/': [
+      '1.advanc',
+      '2.design',
+      '3.dataStructure',
+      '4.algorithm'
+    ],
+    '/senior/network/': [
+      '1.UDP',
+      '2.TCP',
+      '3.HTTPandTLS',
+      '4.HTTP2andHTTP3'
+    ],
+    '/senior/browser/': [
+      '1.cache',
+      '2.render'
+    ],
+    '/2.back-end/': [
+      '1.node',
+      '2.express',
+      '3.koa',
+      '4.egg'
+    ],
+    '/largeFactories/': [
+      '阿里巴巴',
+      '蘑菇街',
+      '饿了么',
+      '今日头条',
+      '滴滴',
+      '兑吧',
+      '百分点',
+      '海风教育',
+      '海康威视',
+      '沪江',
+      '酷家乐',
+      '寺库',
+      '挖财',
+      '网易',
+      '微医',
+      '携程',
+      '喜马拉雅',
+      '有赞'
+    ],
+    '/myInterview/': [
+      '20210131'
+    ]
+  }
+module.exports = sidebar
