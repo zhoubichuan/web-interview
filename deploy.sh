b4a676a3f241f71c0006ad8a0ea242fc44daa61b
周必川 <zhoubichuan@icloud.com>
2023-02-28 23:54:26 +0800

    master

--- deploy.sh
+++ deploy.sh
@@ -3,17 +3,3 @@ git add -A
 git commit -m 'master'
 git pull gitee master
 git push gitee master
-
-git init
-git checkout --orphan gh-pages
-git add .
-git commit -m 'gh-pages'
-git remote add gitee https://gitee.com/zhoubichuan/web-interview.git
-git push -f gitee gh-pages
-
-cd -
-# 1.发布到master分支
-git add -A
-git commit -m 'master'
-git pull github master
-git push github master
